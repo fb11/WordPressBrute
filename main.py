524bfd19ce4ad73999a7584decefdb6e19c4232d
Black Viking <yakuza3003@yandex.com>
2017-04-17 14:07:17 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#
+
+"""
+Wordpress brute force tool with dork maker and dork scanner.
+"""
+
+__author__ = "Black Viking"
+__date__   = "15.04.2017"
+
+import os
+import sys
+import time
+
+sys.path.append("./source")
+
+import dorkMaker
+import dorkScanner
+import bruteForce
+
+def help():
+	pass
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	pass
