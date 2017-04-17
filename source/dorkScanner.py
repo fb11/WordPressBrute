a7a2d69b551cb0640a09b22c8811e51c192df64b
Black Viking <yakuza3003@yandex.com>
2017-04-17 15:20:11 +0300

    find sites from dorks

---
+++ source/dorkScanner.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#
+
+"""
+dorkScanner.py, find sites from dorks.
+
+Usage:
+	~$ python2 dorkScanner.py blackvkng 10
+
+	// 10 site will be here about "blackvkng"
+"""
+
+__author__ = "Black Viking"
+__date__   = "16.04.2017"
+
+
+import sys
+import google
+
+sites = []
+
+def getSites(query, num):
+	''' Get sites from query'''
+
+	for site in google.search(query=query, num=int(num), stop=1):
+		sites.append(site)
+
+if __name__ == "__main__":
+	if len(sys.argv) == 3:
+		query = sys.argv[1]
+		num   = sys.argv[2]
+
+		getSites(query, num)
+
+		print "\n" + "-"*60
+		for site in sites:
+			print site
+		print "\n" + "-"*60
+		print "[+] %s site found!"%(len(sites))
+
+	else:
+		sys.exit()
